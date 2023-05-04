08bdda1455ca586fd237455440b273de56d989a1
yang1206 <yang15188280673@outlook.com>
2023-05-04 09:34:22 +0800

    chore: podfile

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   dependencies: {
+    // ...{ 'react-native-flipper': { platforms: { ios: null } } },
     ...(process.env.NO_FLIPPER ? { 'react-native-flipper': { platforms: { ios: null } } } : {}),
   },
 }
