fe4d94488325dff04f53aaf481ccad52df2932d6
Yang1206 <yangjunwei@Yang1206MacBook-Pro.local>
2023-10-09 09:06:32 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+import 'react-native-gesture-handler';
+
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import * as SplashScreen from 'expo-splash-screen';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import FlashMessage from 'react-native-flash-message';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import { APIProvider } from '@/api';
+import { hydrateAuth } from '@/core';
+import { loadSelectedTheme } from '@/hooks';
+import { RootNavigator } from '@/navigation';
+
+hydrateAuth();
+loadSelectedTheme();
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <APIProvider>
+          <RootNavigator />
+          <FlashMessage position="top" />
+        </APIProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
